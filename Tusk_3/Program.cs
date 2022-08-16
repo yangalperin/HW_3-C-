afe5278827ea45458e3ec4421fd38d912b1cd5f6
Yan <yangalperin@gmail.com>
2022-08-16 07:51:18 +0300

    Third task

---
+++ Tusk_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int number = ReadInt("Введите число: ");
+int i = 1;
+while(i <= number)
+{
+    Console.Write(Cube(i)+ " ");
+    i++;
+}
+
+
+int Cube(int a)
+{
+    return a * a * a;
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+} 
