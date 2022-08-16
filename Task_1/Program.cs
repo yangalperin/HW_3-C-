56782f8218e0571082894f0d5bd325ebe1de86d6
yangalperin <109693059+yangalperin@users.noreply.github.com>
2022-08-16 14:45:58 +0300

    First task

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -4,7 +4,7 @@
 //23432 -> да
 Console.Write("Введите пятизначное число: ");
 string numbers = Console.ReadLine();
-if (numbers[0] == numbers[4] && numbers[1] == numbers[3])
+if (numbers[0] == numbers[4] && numbers[1] ==numbers[3])
 Console.WriteLine("Число " + numbers + " является палиндромом");
 else
 {
